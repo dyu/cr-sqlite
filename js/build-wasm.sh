266065a058aa70cff55d66b5ec7770e3aef620f6
dyu <david.yu.ftw@gmail.com>
2023-03-21 21:20:37 +0800

    use existing emsdk installation if available

--- js/build-wasm.sh
+++ js/build-wasm.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+emsdk_activate() {
+    cd deps/emsdk
+    ./emsdk install latest
+    ./emsdk activate latest
+    source ./emsdk_env.sh
+    cd - > /dev/null
+}
+
 mkdir -p packages/crsqlite-wasm/dist
-cd deps/emsdk
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh
-cd ../wa-sqlite
+command -v emcc > /dev/null 2>&1 || emsdk_activate
+cd deps/wa-sqlite
 make
 cp dist/wa-sqlite-async.wasm ../../packages/crsqlite-wasm/dist/crsqlite.wasm
 cp dist/wa-sqlite-async.mjs ../../packages/crsqlite-wasm/src/crsqlite.mjs
