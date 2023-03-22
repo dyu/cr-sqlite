fd97cbb290a4ad23155e0ebe1d41aed6a59fd60d
dyu <david.yu.ftw@gmail.com>
2023-03-22 09:16:26 +0800

    no copy

--- js/build-wasm.sh
+++ js/build-wasm.sh
@@ -12,5 +12,5 @@ mkdir -p packages/crsqlite-wasm/dist
 command -v emcc > /dev/null 2>&1 || emsdk_activate
 cd deps/wa-sqlite
 make
-cp dist/wa-sqlite-async.wasm ../../packages/crsqlite-wasm/dist/crsqlite.wasm
-cp dist/wa-sqlite-async.mjs ../../packages/crsqlite-wasm/src/crsqlite.mjs
+#cp dist/wa-sqlite-async.wasm ../../packages/crsqlite-wasm/dist/crsqlite.wasm
+#cp dist/wa-sqlite-async.mjs ../../packages/crsqlite-wasm/src/crsqlite.mjs
